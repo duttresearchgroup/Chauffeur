9c9b3b715a83d5de2a5104267dc5333949bdac4c
DongDongJu <commisori28@gmail.com>
2021-03-27 13:39:19 -0700

    Fix incorrect path for binaries

--- scripts/openMVG/build.sh
+++ scripts/openMVG/build.sh
@@ -2,8 +2,10 @@
 
 if [[ $# -eq 0 ]];then
 	echo "usage : $0 [px2/tx2]"
+	exit 1
 fi
 
 if [[ $1 -eq "px2" ]];then
-	cd ../../applications/openMVG/openMVG;mkdir build;cd build;cmake -DCMAKE_BUILD_TYPE=RELEASE ../src/;make -j$(nproc)	
+	cd ../../applications/openMVG/openMVG;mkdir build;cd build;cmake -DCMAKE_BUILD_TYPE=RELEASE ../src/;make -j$(nproc)
+	mv Linux-aarch64-RELEASE/* software/SfM/.
 fi
