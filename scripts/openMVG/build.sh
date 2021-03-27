a3416a92b96cea191e8d3881581d6cdb16fcfb2f
DongDongJu <commisori28@gmail.com>
2021-03-27 02:29:43 -0700

    Add script for local build

---
+++ scripts/openMVG/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]];then
+	echo "usage : $0 [px2/tx2]"
+fi
+
+if [[ $1 -eq "px2" ]];then
+	cd ../../applications/openMVG/openMVG;mkdir build;cd build;cmake -DCMAKE_BUILD_TYPE=RELEASE ../src/;make -j$(nproc)	
+fi
