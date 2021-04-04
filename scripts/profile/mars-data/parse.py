148df3411213f26a700bc26f778cee9c19c56f0a
lemingc <lemingc@uci.edu>
2021-04-03 17:44:51 -0700

    update for parse.py

--- scripts/profile/mars-data/parse.py
+++ scripts/profile/mars-data/parse.py
@@ -1,6 +1,37 @@
-1. Loop all the files and select the ones which end in (-fine_trace.csv)
-2. Extract the application name -- first part (e.g., jetson_inference)
+#1. Loop all the files and select the ones which end in (-fine_trace.csv)
+#2. Extract the application name -- first part (e.g., jetson_inference)
 
-    3. For each application extract <avg> power_cpu_w, power_gpu_w, power_mem_w
+#    3. For each application extract <avg> power_cpu_w, power_gpu_w, power_mem_w
     
-4. Create a new CSV (energy-results.csv) with Appname, <avg> power_cpu_w, <avg> power_gpu_w, <avg> power_mem_w
+#4. Create a new CSV (energy-results.csv) with Appname, <avg> power_cpu_w, <avg> power_gpu_w, <avg> power_mem_w
+
+import sys
+import os
+import numpy as np
+import pandas as pd
+
+def find_files(dir) -> list:
+    result = []
+    for filename in os.listdir(dir):
+        if filename.endswith("-fine_trace.csv"):
+            result.append(str(filename))
+    return result
+
+def process(files:list) -> dict:
+    output = []
+    for i in files:
+        data = pd.read_csv(i,delimiter=";")
+        name = i[:-15].replace("_", "-")
+        output.append([name, data["power_cpu_w"].mean(), data["power_gpu_w"].mean(),data["power_mem_w"].mean()])
+    return output
+
+def write_file(data:list):
+    col = ['ApplicationName', 'Cpu', 'Gpu', 'Mem']
+    new_data = pd.DataFrame(data, columns = col)
+    new_data.to_csv("energy-result.csv", index=False)
+
+
+if __name__ == "__main__":
+    FileList = find_files( str(sys.argv[1]) )
+    DataOutput = process(FileList)
+    write_file(DataOutput)
