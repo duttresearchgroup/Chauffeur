55ebdfbba9d115ce68bd5c1a7d999966fdf39664
Biswadip Maity <biswadip.maity@gmail.com>
2021-06-11 20:02:50 -0700

    Update cross compiler for new applications

--- docker/build.sh
+++ docker/build.sh
@@ -8,14 +8,14 @@ source="/workspace/applications"
 
 #   Build Jetson-Inference
 build_jetson_inference () {
-    mkdir -p $target/jetson-inference && cd $target/jetson-inference
+    mkdir -p $target/object_detection/jetson-inference && cd $target/object_detection/jetson-inference
     rm -rf build && mkdir -p build && cd build
     cmake \
         -DCUDA_nppicc_LIBRARY=/usr/local/cuda/targets/aarch64-linux/lib/stubs/libnppicc.so \
         -DBUILD_INTERACTIVE=NO \
-        $source/jetson-inference
+        $source/object_detection/jetson-inference
     make -j"$(grep ^processor /proc/cpuinfo | wc -l)" 
-    cd $target/jetson-inference/build/aarch64/bin 
+    cd $target/object_detection/jetson-inference/build/aarch64/bin 
     target_images=$(readlink images)
     target_networks=$(readlink networks)
     rm -rf images && rm -rf networks
@@ -24,32 +24,32 @@ build_jetson_inference () {
 
 # Build Kalman filter
 build_kalman_filter() {
-    mkdir -p $target/Kalman-Filter && cd $target/Kalman-Filter
+    mkdir -p $target/kalman_filter/extended-kalman-filter && cd $target/kalman_filter/extended-kalman-filter
     rm -rf build && mkdir -p build && cd build
-    cmake $source/Kalman-Filter
+    cmake $source/kalman_filter/extended-kalman-filter
     make -j"$(grep ^processor /proc/cpuinfo | wc -l)" 
 }
 
 # Build Lane Detection
 build_lane_detection() {
-    mkdir -p $target/lane_detection && cd $target/lane_detection
+    mkdir -p $target/lane_detection/jetson-lane-detection && cd $target/lane_detection/jetson-lane-detection
     rm -rf build && mkdir -p build && cd build
-    cmake $source/lane_detection
+    cmake $source/lane_detection/jetson-lane-detection
     make -j"$(grep ^processor /proc/cpuinfo | wc -l)" 
-    mv $source/lane_detection/bin $target/lane_detection/bin
+    mv $source/lane_detection/jetson-lane-detection/bin $target/lane_detection/bin
     # -DJETSON_TX2=ON \
     # -DOpenCV_DIR=/usr/local/opencv2/share/OpenCV/OpenCVConfig-version.cmake \
 }
 
 build_cuda_lane_detection() {
     # nvcc -gencode arch=compute_62,code=sm_62 -I/usr/local/include/opencv4 -L/usr/local/lib/ *.cpp *cu -lopencv_core -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_videoio -std=c++11 -o $source/bin/cudaLaneDetection
-    cd $source/cuda-lane-detection
+    cd $source/lane_detection/cuda-lane-detection
     patch -b -p1 < $source/patches/cuda-lane-detection.patch
-    mkdir -p $target/cuda-lane-detection && cd $target/cuda-lane-detection
+    mkdir -p $target/lane_detection/cuda-lane-detection && cd $target/lane_detection/cuda-lane-detection
     rm -rf build && mkdir -p build && cd build
-    cmake $source/cuda-lane-detection
+    cmake $source/lane_detection/cuda-lane-detection
     make -j"$(grep ^processor /proc/cpuinfo | wc -l)" 
-    cd $source/cuda-lane-detection
+    cd $source/lane_detection/cuda-lane-detection
     patch -R -p1 < $source/patches/cuda-lane-detection.patch
     rm -rf *.orig
 }
@@ -59,7 +59,7 @@ build_cuda_lane_detection() {
 build_openmvg() {
     mkdir -p $target/openMVG/openMVG && cd $target/openMVG/openMVG
     rm -rf build && mkdir -p build && cd build
-    cmake -DOpenMVG_USE_OCVSIFT=ON $source/openMVG/openMVG/src
+    cmake -DOpenMVG_USE_OCVSIFT=ON $source/structure_from_motion/open-mvg/src
     make -j"$(grep ^processor /proc/cpuinfo | wc -l)" 
     mv Linux-aarch64-Release/* software/SfM/.
 }
@@ -88,13 +88,13 @@ build_darknet_ros() {
 # Build 3 FLOAM
 build_floam() {
     source /opt/ros/melodic/setup.sh
-    mkdir -p $target/odometry/floam && cd $target/odometry/floam
+    mkdir -p $target/localization_and_mapping/floam && cd $target/localization_and_mapping/floam
     rm -rf devel && rm -rf build && rm -rf install
     mkdir -p build && cd build
-    cmake $source/odometry/floam \
+    cmake $source/localization_and_mapping/floam \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCATKIN_DEVEL_PREFIX=$target/odometry/floam/devel \
-        -DCMAKE_INSTALL_PREFIX=$target/odometry/install \
+        -DCATKIN_DEVEL_PREFIX=$target/localization_and_mapping/floam/devel \
+        -DCMAKE_INSTALL_PREFIX=$target/localization_and_mapping/install \
         -DBUILD_SHARED_LIBS=OFF
 
     make -j"$(grep ^processor /proc/cpuinfo | wc -l)" 
@@ -122,8 +122,54 @@ build_path_planning() {
 
 
 build_lanenet_lane_detection() {
-    rm -rf $target/lanenet-lane-detection
-    cp -r $source/lanenet-lane-detection $target/lanenet-lane-detection
+    rm -rf $target/lane_detection/lanenet-lane-detection
+    cp -r $source/lane_detection/lanenet-lane-detection $target/lane_detection/lanenet-lane-detection
+}
+
+build_lidar_tracking() {
+    rm -rf $target/object_tracking/lidar-tracking
+    source /opt/ros/melodic/setup.sh
+    mkdir -p $target/object_tracking/lidar-tracking && cd $target/object_tracking/lidar-tracking
+    rm -rf devel && rm -rf build
+    mkdir -p build && cd build
+    cmake $source/object_tracking/lidar-tracking \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCATKIN_DEVEL_PREFIX=$target/object_tracking/lidar-tracking/devel \
+        -DCMAKE_INSTALL_PREFIX=$target/object_tracking/lidar-tracking/install \
+        -DBUILD_SHARED_LIBS=OFF
+
+    make -j"$(grep ^processor /proc/cpuinfo | wc -l)" install
+    rm -rf $target/object_tracking/lidar-tracking/devel && \
+        rm -rf $target/object_tracking/lidar-tracking/build
+    
+}
+
+build_lidar_tracking() {
+    rm -rf $target/object_tracking/lidar-tracking
+    source /opt/ros/melodic/setup.sh
+    mkdir -p $target/object_tracking/lidar-tracking && cd $target/object_tracking/lidar-tracking
+    rm -rf devel && rm -rf build
+    mkdir -p build && cd build
+    cmake $source/object_tracking/lidar-tracking \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCATKIN_DEVEL_PREFIX=$target/object_tracking/lidar-tracking/devel \
+        -DCMAKE_INSTALL_PREFIX=$target/object_tracking/lidar-tracking/install \
+        -DBUILD_SHARED_LIBS=OFF
+
+    make -j"$(grep ^processor /proc/cpuinfo | wc -l)" install
+    rm -rf $target/object_tracking/lidar-tracking/devel && \
+        rm -rf $target/object_tracking/lidar-tracking/build
+    
+}
+
+build_orb_slam_3() {
+    rm -rf $source/localization_and_mapping/orb-slam-3/Thirdparty/DBoW2/build
+    rm -rf $source/localization_and_mapping/orb-slam-3/Thirdparty/g2o/build
+    rm -rf $source/localization_and_mapping/orb-slam-3/Vocabulary/build
+    rm -rf $source/localization_and_mapping/orb-slam-3/build
+
+    cd $source/localization_and_mapping/orb-slam-3
+    bash build.sh
 }
 
 build_jetson_inference
@@ -135,3 +181,5 @@ build_openmvg
 build_darknet_ros
 build_floam
 build_path_planning
+build_lidar_tracking
+build_orb_slam_3
