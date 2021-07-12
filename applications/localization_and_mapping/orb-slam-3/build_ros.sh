d8fbbd4fc0150c112529925901b7e94d9189fe68
Bryan Donyanavard <bryandony@gmail.com>
2021-07-12 17:03:57 +0200

    add ros build to os3 build script

--- applications/localization_and_mapping/orb-slam-3/build_ros.sh
+++ applications/localization_and_mapping/orb-slam-3/build_ros.sh
@@ -1,6 +1,5 @@
 echo "Building ROS nodes"
 
-export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:$PWD/ORB_SLAM3/Examples/ROS
 cd Examples/ROS/ORB_SLAM3
 mkdir build
 cd build
--- scripts/localization/orb-slam-3/build.sh
+++ scripts/localization/orb-slam-3/build.sh
@@ -11,5 +11,8 @@ if [[ $1 == "tx2" ]]; then
 	cd $R_SRC_ROOT/applications/localization_and_mapping/orb-slam-3
 	chmod +x build.sh
 	bash build.sh
+	export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:$PWD/ORB_SLAM3/Examples/ROS
+	chmod +x build_ros.sh
+	bash build_ros.sh
 
 fi
