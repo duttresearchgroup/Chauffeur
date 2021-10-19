deb78fb86c4783fcf7af0e0eabfeed86e1c64df1
DongDongJu <commisori28@gmail.com>
2021-10-19 16:06:31 -0700

    Change the log path for extended kalman filter

--- scripts/kalman_filter/extended-kalman-filter/run_infinite.sh
+++ scripts/kalman_filter/extended-kalman-filter/run_infinite.sh
@@ -6,5 +6,5 @@ source $DIR/../../envs.sh
 cd $EXTENDED_KALMAN_FILTER_BINARY_FOLDER
 while [ 1 ]
 do
-	{ time ./$EXTENDED_KALMAN_FILTER_BINARY_NAME $EXTENDED_KALMAN_FILTER_DATA_FOLDER/sample-laser-radar-measurement-data-1.txt output.txt >> /workspace/logs/kf.txt 2>&1 ; } 2>> time_kf.txt
+	{ time ./$EXTENDED_KALMAN_FILTER_BINARY_NAME $EXTENDED_KALMAN_FILTER_DATA_FOLDER/sample-laser-radar-measurement-data-1.txt output.txt >> /workspace/logs/kf.txt 2>&1 ; } 2>> /workspace/logs/time_kf.txt
 done
